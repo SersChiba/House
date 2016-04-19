e4d6935530afb78ba76963a05825b1a23ff6948b
Andris Stalbovs <Andris.Stalbovs@squaliocc.com>
2016-04-19 16:05:38 +0300

    taisām interfaces un inheritance

--- HouseProject/Outside.cs
+++ HouseProject/Outside.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace HouseProject
 {
-    class Outside
+    class Outside:Location
     {
+        private bool hot;
+        public bool Hot { get { return hot; } }
+        public Outside(string name, bool hot) : base(name)
+        {
+            this.hot = hot;
+        }
+        public override string Description
+        {
+            get
+            {
+                if(Hot)
+                return "It's very hot here.";
+                return base.Description;
+            }
+        }
+    }
+    class OutsideWithDoor : Outside, IHasExteriorDoor
+    {
+        public OutsideWithDoor(string name, bool hot) : base(name, hot)
+        {
+        }
+
+        public string DoorDescription
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Location DoorLocation
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- HouseProject/Room.cs
+++ HouseProject/Room.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace HouseProject
 {
-    class Room
+    class Room : Location
     {
+        private string decoration;
+        public string Decoration { get { return decoration; } }
+        public Room(string name, string decoration) : base(name)
+        {
+            this.decoration = decoration;
+        }
+        public override string Description
+        {
+            get
+            {
+                return "You see the " + Decoration;
+            }
+        }
+    }
+
+    class RoomWithDoor : Room, IHasExteriorDoor
+    {
+        public RoomWithDoor(string name, string decoration) : base(name, decoration)
+        {
+        }
+
+        public string DoorDescription
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Location DoorLocation
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
